ebffa9575480eb0f24ff56dad888f41dd643a388
sagar <sagardabas2019@gmail.com>
2025-08-11 21:06:51 +0530

    added the cursor effect ad finalized the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./Components/Header"
 import HERO from "./Components/HERO.JSX"
+import Cursor from "./Components/Cursor"
 
 
 export default function app() {
   return (
     <>
       <Header /> 
-      <HERO/>
+      <HERO />
+      <Cursor/>
     </>
   )
 }
